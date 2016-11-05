d14827480ec830f3ed35c9fbe8de503a8f786c1e
jumpegg <jumpegg@naver.com>
2016-11-06 02:23:00 +0900

    first commit

---
+++ polls/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def results(request, question_id):
+    response= "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoseNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
